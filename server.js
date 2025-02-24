9aaac4603b866b4395daaaf35c85b6809abbddcc
vaibhav-csfreak <vs12022001@gmail.com>
2025-02-24 21:20:07 +0530

    fixed the app listen issue

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 const { CONTACT } = require('./Resources/data');
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 
 app.get('/contact', (req, res) => {
     res.json(CONTACT)
 })
-app.listen(5000 || PORT, () => { console.log('listening on 5000') })
+app.listen(PORT, () => { console.log('listening on 5000') })
